af7cf402a3975a2863df152031e70c0076cd7e30
mitchmcdee <mitchmcdee@gmail.com>
2018-07-14 23:19:41 +1000

    fleshed out api end points and added ajax calls

--- navitron/static/js/index.js
+++ navitron/static/js/index.js
@@ -14,8 +14,15 @@ let MIDDLEMOUNT_BOTTOM = MIDDLEMOUNT_TOP - MIDDLEMOUNT_OVERLAY_HEIGHT * MIDDLEMO
 let MIDDLEMOUNT_ZONE_NUM = 55;
 let MIDDLEMOUNT_ZONE_LETTER = 'K';
 
+let HEATMAP_RATE = 100;
+let EQUIPMENT_RATE = 100;
+
 var overlay;
 var map;
+var equipment;
+var heatmap_points = new google.maps.MVCArray();
+
+// Setup middlemount mine on google maps
 function init_map() {
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 14,
@@ -40,9 +47,31 @@ function init_map() {
     overlay = new MiddlemountOverlay(bounds, MIDDLEMOUNT_OVERLAY_SRC, map);
 };
 
+// Get new heatmap points
+function get_new_heatmap_points() {
+    // Send AJAX query
+    $.ajax({ url: "getNewHeatmapPoints", type: "POST" }).done(res => {
+        console.log('got response from getNewHeatmapPoints');
+    });
+    // Delay and repeat function
+    setTimeout(get_new_heatmap_points, HEATMAP_RATE);
+};
+
+// Get current equipment points
+function get_current_equipment_points() {
+    // Send AJAX query
+    $.ajax({ url: "getCurrentEquipmentPoints", type: "POST" }).done(res => {
+        console.log('got response from getCurrentEquipmentPoints');
+    });
+    // Delay and repeat function
+    setTimeout(get_current_equipment_points, EQUIPMENT_RATE);
+};
+
 // Set up
 google.maps.event.addDomListener(window, 'load', function() {
     init_map();
+    get_new_heatmap_points();
+    get_current_equipment_points();
 });
 
 
