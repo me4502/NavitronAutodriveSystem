58577d4a3a1e8c685ac9a6da1ac26bc4917871aa
Matthew Miller <mnmiller1@me.com>
2018-07-14 13:27:24 +1000

    Use Jinja

--- src/main/java/com/me4502/navitron/NavitronAutodriveSystem.java
+++ src/main/java/com/me4502/navitron/NavitronAutodriveSystem.java
@@ -2,11 +2,23 @@ package com.me4502.navitron;
 
 import static spark.Spark.get;
 import static spark.Spark.port;
+import static spark.Spark.staticFiles;
+
+import spark.ModelAndView;
+import spark.template.jinjava.JinjavaEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class NavitronAutodriveSystem {
 
     public static void main(String[] args) {
         port(8080);
+        staticFiles.location("/public");
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.html");
+        }, new JinjavaEngine());
         get("/hello", (req, res) -> "Hello World");
     }
 }
