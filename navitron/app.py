30c4f839ab1f69419a7e99c828e81694ee00a757
mitchmcdee <mitchmcdee@gmail.com>
2018-07-15 03:13:18 +1000

    added icon markers for trucks and shovels. added test data

--- navitron/app.py
+++ navitron/app.py
@@ -1,24 +1,40 @@
+import time
+import random
 from waitress import serve
 from flask import Flask, render_template, jsonify
 
 
-app = Flask(__name__)
+NAVITRON_APP = Flask(__name__)
+
+# TODO(mitch): turn these test points into something legit
+num_points = 100
+equipment_index = -1
+equipment = {
+    0: [('truck',
+         -23.025 - i * 0.0002,
+         148.74 + i * 0.0002,
+         100 + 1000 * (random.random() - 0.1))
+        for i in range(num_points)],
+    1: [('shovel',
+         -23.025 + 0.001 * (random.random() - 0.1),
+         148.74 + 0.1 * (random.random() - 0.5))] * num_points,
+    2: [('shovel',
+         -23.025 + 0.001 * (random.random() - 0.1),
+         148.74 + 0.1 * (random.random() - 0.5))] * num_points
+}
 
 
-@app.route("/")
+@NAVITRON_APP.route("/")
 def index():
     return render_template('index.html')
 
 
-@app.route("/getNewHeatmapPoints", methods=['POST'])
-def get_current_heatmap_points():
-    return "TODO(mitch): this"
-
-
-@app.route("/getCurrentEquipmentPoints", methods=['POST'])
-def get_current_equipment_points():
-    return "TODO(mitch): this"
+@NAVITRON_APP.route("/getEquipmentUpdate", methods=['POST'])
+def get_equipment_update():
+    global equipment_index
+    equipment_index += 1
+    return jsonify({k: v[equipment_index % num_points] for k, v in equipment.items()})
 
 
 if __name__ == "__main__":
-    serve(app, port=80)
+    serve(NAVITRON_APP, port=80)
