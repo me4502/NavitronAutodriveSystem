aedca44f2994bde273102cd7ed4f5af834891434
mitchmcdee <mitchmcdee@gmail.com>
2018-07-14 23:10:36 +1000

    added api end points and cleaned up js

--- navitron/app.py
+++ navitron/app.py
@@ -1,5 +1,5 @@
 from waitress import serve
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 
 app = Flask(__name__)
@@ -8,6 +8,16 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route("/getCurrentHeatmapPoints", methods=['POST'])
+def get_current_heatmap_points():
+    return "TODO(mitch): this"
+
+
+@app.route("/getCurrentEquipmentPoints", methods=['POST'])
+def get_current_equipment_points():
+    return "TODO(mitch): this"
 
 
 if __name__ == "__main__":
